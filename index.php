a42a550a39ae91a9565388018d1c13c2ab766a41
qmak-byte <blithesrm102@gmail.com>
2021-02-07 10:11:01 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello world</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem obcaecati, sed commodi illum hic, quidem voluptatem minus quas, laudantium praesentium adipisci cumque facilis possimus rem. Enim ducimus beatae quo deleniti?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
