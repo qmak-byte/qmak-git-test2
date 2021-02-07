84d2c438615f74e0334b2aac5a08fb125c6f748e
qmak-byte <blithesrm102@gmail.com>
2021-02-07 10:05:15 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello world</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem obcaecati, sed commodi illum hic, quidem voluptatem minus quas, laudantium praesentium adipisci cumque facilis possimus rem. Enim ducimus beatae quo deleniti?</p>
 </body>
 </html>
